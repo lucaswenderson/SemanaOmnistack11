13e745a87b0a54096a5256c503ba68be49cd7508
Luk4zW3 <lucaswenderson1000@gmail.com>
2020-04-06 18:48:50 -0300

    Mobile da aplicação

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -12,18 +12,35 @@ import styles from './styles';
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
+  
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const navigation = useNavigation();
 
-  function navigateToDetail() {
-    navigation.navigate('Detail');
+  function navigateToDetail( incident ) {
+    navigation.navigate('Detail', { incident });
   }
 
   async function loadIncidents() {
-    const response = await api.get('incidents');
+    if(loading) {
+      return;
+    }
+
+    if( total > 0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get('incidents', {
+      params: { page }
+    });
     
-    setIncidents(response.data);
+    setIncidents([...incidents, ...response.data]);
     setTotal(response.headers['x-total-count']);
+    setPage(page + 1);
+    setLoading(false);
   }
   
   useEffect(() => {
@@ -47,6 +64,8 @@ export default function Incidents() {
         style={styles.incidentList}
         keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
@@ -64,7 +83,7 @@ export default function Incidents() {
             </Text>
             <TouchableOpacity
               style={styles.detailsButton}
-              onPress={navigateToDetail}
+              onPress={() => navigateToDetail(incident)}
             >
               <Text style={styles.detailsButtonText}>Ver mais detalhes </Text>
               <Feather name="arrow-right" size={16} color="#E02041" /> 
